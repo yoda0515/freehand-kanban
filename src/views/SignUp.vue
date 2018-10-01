313fe720b41bc1e28ee67683e90fe8d10671dde3
d6ms <g.denden.ms@gmail.com>
2018-10-01 22:02:14 +0900

    add sign up route

---
+++ src/views/SignUp.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>sign up</h1>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class SignUp extends Vue {
+
+  @Prop()
+  private msg!: string;
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
