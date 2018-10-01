28e4400d7cf372697f73a7d95e07cbcd16187055
d6ms <g.denden.ms@gmail.com>
2018-10-02 02:27:52 +0900

    introduced firebase

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,20 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import firebase from 'firebase/app';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDERID,
+};
+firebase.initializeApp(config);
 
 new Vue({
   router,
