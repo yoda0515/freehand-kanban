ca7a5aca4c8e208e9d70baae0c9d652c5a44eb11
d6ms <g.denden.ms@gmail.com>
2018-10-12 00:05:20 +0900

    introduced fabric.js and add some simple implementations

--- src/views/todo/DashBoard.vue
+++ src/views/todo/DashBoard.vue
@@ -1,33 +1,51 @@
 <template>
   <div class="dashboard">
     <h1>dashboard works</h1>
-    <template v-for="todo in todos">
-      {{todo}}
-    </template>
+    <canvas id="title" width="500" height="100" style="border: 1px solid #000000;"></canvas>
+    <button type="button" class="btn btn-primary" v-on:click="exportSvg()">export</button>
+    <span>{{svg}}</span>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import firestore from '../../firebaseInit';
+import { fabric } from 'fabric';
 
 @Component
 export default class DashBoard extends Vue {
 
   private todos: any[];
+  private canvas: any;
+  private svg: string;
 
   public data() {
     return {
-      todos: []
+      todos: [],
+      svg: ''
     };
   }
 
-  public created() {
+  public mounted(): void {
     firestore.collection('todos').get().then(querySnapshot => {
       querySnapshot.forEach(doc => {
         this.todos.push(doc.data());
       });
+    }).then(() => {
+      this.canvas = new fabric.Canvas('title', {
+        isDrawingMode: true,
+        selection: true,
+        stateful: true
+      });
+      fabric.loadSVGFromString(this.todos[0].content, (objects, options) => {
+        const obj = fabric.util.groupSVGElements(objects, options);
+        this.canvas.add(obj).renderAll();
+      });
     });
+  }
+
+  public exportSvg() {
+    this.svg = this.canvas.toSVG();
   }
 }
 </script>
