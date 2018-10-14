1871d3af7d645b8778a0f718871f4caa2c87d1a4
d6ms <g.denden.ms@gmail.com>
2018-10-15 02:35:04 +0900

    implemented editor canvas

--- src/components/todo/ToDoEditor.vue
+++ src/components/todo/ToDoEditor.vue
@@ -1,8 +1,34 @@
 <template>
+  <div class="todo-editor">
+    <canvas id="canvas-title" width="500" height="100" style="border: 1px solid #000000;"></canvas>
+  </div>
 </template>
 
 <script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { fabric } from 'fabric';
+import { Todo } from '@/components/todo/ToDoList.vue';
 
+@Component({})
+export default class ToDoEditor extends Vue {
+
+  @Prop()
+  private todo: Todo;
+  private titleCanvas: any;
+
+  public mounted(): void {
+    this.titleCanvas = new fabric.Canvas('canvas-title', {
+      isDrawingMode: true,
+      selection: true,
+      stateful: true
+    });
+    // TODO 実際はcontentでなくtitle
+    fabric.loadSVGFromString(this.todo.content, (objects, options) => {
+      const obj = fabric.util.groupSVGElements(objects, options);
+      this.titleCanvas.add(obj).renderAll();
+    });
+  }
+}
 </script>
 
 <style lang="sass" scoped>
--- src/views/todo/DashBoard.vue
+++ src/views/todo/DashBoard.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="dashboard">
     <h1>dashboard works</h1>
-    <canvas id="title" width="500" height="100" style="border: 1px solid #000000;"></canvas>
     <button type="button" class="btn btn-primary" v-on:click="exportSvg()">export</button>
     <span>{{svg}}</span>
     <to-do-list
@@ -10,7 +9,9 @@
       v-on:deleteClick="deleteItem">
     </to-do-list>
     <modal v-if="showModal">
-      <h3 slot="body">custom body</h3>
+      <template slot="body">
+        <to-do-editor v-bind:todo="editing"></to-do-editor>
+      </template>
       <template slot="footer">
         <button class="btn btn-primary modal-default-button" v-on:click="closeDetail()">save</button>
       </template>
@@ -23,18 +24,19 @@ import { Component, Vue } from 'vue-property-decorator';
 import firestore from '../../firebaseInit';
 import { fabric } from 'fabric';
 import ToDoList, { Todo } from '@/components/todo/ToDoList.vue';
+import ToDoEditor from '@/components/todo/ToDoEditor.vue';
 import Modal from '@/components/vendor/Modal.vue';
 
 @Component({
   components: {
     ToDoList,
+    ToDoEditor,
     Modal
   }
 })
 export default class DashBoard extends Vue {
 
   private todos: Todo[];
-  private canvas: any;
   private svg: string;
   private showModal: boolean;
   private editing: Todo;
@@ -53,21 +55,10 @@ export default class DashBoard extends Vue {
         this.todos.push(doc.data() as Todo);
       });
     });
-    // .then(() => {
-    //   this.canvas = new fabric.Canvas('title', {
-    //     isDrawingMode: true,
-    //     selection: true,
-    //     stateful: true
-    //   });
-    //   fabric.loadSVGFromString(tmp, (objects, options) => {
-    //     const obj = fabric.util.groupSVGElements(objects, options);
-    //     this.canvas.add(obj).renderAll();
-    //   });
-    // });
   }
 
   public exportSvg() {
-    this.svg = this.canvas.toSVG();
+    // this.svg = this.canvas.toSVG();
   }
 
   public openDetail(index: number): void {
